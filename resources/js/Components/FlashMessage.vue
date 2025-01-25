be605d1734e89f5eca1423ed28c3b64c59ec8f57
alvinseptiano <spazmantiz.dev@gmail.com>
2025-01-25 21:16:34 +0700

    fix toaster

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -1,35 +1,5 @@
-<!-- <script setup>
-import { usePage } from '@inertiajs/vue3';
-import { ref, watch } from 'vue';
-
-const page = usePage();
-const showToast = ref(false);
-
-// Watch the flash message and set the toast visibility
-// watch(
-//     () => page.props.flash.message,
-//     (message) => {
-//         if (message) {
-//             // await nextTick();
-//             showToast.value = true;
-//             setTimeout(() => {
-//                 showToast.value = false;
-//             }, 40000); // 3 seconds
-//         }
-//     },
-//     { immediate: true },
-// );
-</script>
-
-<template>
-    <div v-if="showToast" class="toast toast-top toast-end z-50">
-        <div class="alert alert-info">
-            <span>{{ page.props.flash.message }}</span>
-        </div>
-    </div>
-</template> -->
-<!-- Toast Component -->
 <script setup>
+import { ExclamationCircleIcon } from '@heroicons/vue/24/solid';
 defineProps({
     show: Boolean,
     message: String,
@@ -38,8 +8,9 @@ defineProps({
 
 <template>
     <div v-if="show" class="toast toast-top toast-end z-50">
-        <div class="alert alert-info min-h-24 min-w-[300px]">
-            <span>{{ message }}</span>
+        <div class="alert font-bold">
+            <ExclamationCircleIcon class="size-5" />
+            <span> {{ message }}</span>
         </div>
     </div>
 </template>
